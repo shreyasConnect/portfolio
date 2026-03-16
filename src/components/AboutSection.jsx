e158461c0e8435ff46b233f6ea8204172e969fd3
shreyasConnect <shreyas18.dev@gmail.com>
2026-03-16 22:03:35 +0530

    Added resume pdf

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -32,7 +32,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-  href="/resume.pdf"
+  href="public/projects/resume.pdf"
   download
   className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
 >
